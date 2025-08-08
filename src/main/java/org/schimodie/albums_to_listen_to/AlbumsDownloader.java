f85704d8eb9ffa60b48917364dc7a98174d3a0f6
Radu Gagos <radu.gagos@gmail.com>
2025-08-09 00:47:46 +0300

    refactor: migrate MetalstormClient from JSoup to Playwright

--- src/main/java/org/schimodie/albums_to_listen_to/AlbumsDownloader.java
+++ src/main/java/org/schimodie/albums_to_listen_to/AlbumsDownloader.java
@@ -7,10 +7,6 @@ import org.schimodie.albums_to_listen_to.database.Storage;
 import org.schimodie.albums_to_listen_to.database.StorageFileName;
 
 import java.time.Instant;
-import java.time.ZoneOffset;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeFormatterBuilder;
-import java.time.temporal.ChronoField;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -19,7 +15,6 @@ public class AlbumsDownloader {
     @Getter
     private final String fileName;
     private final Instant afterDate;
-    private final MetalstormClient msClient;
 
     public AlbumsDownloader(String fileNamePrefix, Instant afterDate) {
         Objects.requireNonNull(fileNamePrefix);
@@ -27,20 +22,21 @@ public class AlbumsDownloader {
 
         this.afterDate = afterDate;
         this.fileName = StorageFileName.createFileName(fileNamePrefix, afterDate);
-        this.msClient = new MetalstormClient();
     }
 
     public void downloadAlbums() {
-        int page = 1;
-        Instant dateOfLastAlbumOnThePage;
-        List<Album> albums = new ArrayList<>();
+        try (MetalstormClient msClient = new MetalstormClient()) {
+            int page = 1;
+            Instant dateOfLastAlbumOnThePage;
+            List<Album> albums = new ArrayList<>();
 
-        do {
-            albums.addAll(msClient.getAlbums(page++));
-            dateOfLastAlbumOnThePage = msClient.getAlbumDate(albums.get(albums.size() - 1).getAlbumId());
-            albums.get(albums.size() - 1).setDate(dateOfLastAlbumOnThePage);
-        } while (dateOfLastAlbumOnThePage.isAfter(afterDate) || dateOfLastAlbumOnThePage.equals(afterDate));
+            do {
+                albums.addAll(msClient.getAlbums(page++));
+                dateOfLastAlbumOnThePage = msClient.getAlbumDate(albums.getLast().getAlbumId());
+                albums.getLast().setDate(dateOfLastAlbumOnThePage);
+            } while (dateOfLastAlbumOnThePage.isAfter(afterDate) || dateOfLastAlbumOnThePage.equals(afterDate));
 
-        Storage.writeAlbums(albums, fileName);
+            Storage.writeAlbums(albums, fileName);
+        }
     }
 }
--- src/main/java/org/schimodie/albums_to_listen_to/client/MetalstormClient.java
+++ src/main/java/org/schimodie/albums_to_listen_to/client/MetalstormClient.java
@@ -1,16 +1,15 @@
 package org.schimodie.albums_to_listen_to.client;
 
-import org.jsoup.Connection;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.ElementHandle;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
 import org.schimodie.albums_to_listen_to.bean.Album;
 import org.schimodie.common.utils.ExponentialBackoffRetryable;
 import org.schimodie.common.utils.Retryable;
 
-import java.io.IOException;
-import java.net.CookieStore;
 import java.time.Instant;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
@@ -18,21 +17,19 @@ import java.time.format.DateTimeFormatterBuilder;
 import java.time.temporal.ChronoField;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
-public class MetalstormClient {
-    private static final Retryable<Document> RETRY_STRATEGY = new ExponentialBackoffRetryable<>(5, 1000);
+public class MetalstormClient implements AutoCloseable {
+    private static final Retryable<String> RETRY_STRATEGY = new ExponentialBackoffRetryable<>(5, 1000);
     private static final Map<String, String> DEFAULT_HEADERS = Map.ofEntries(
             Map.entry("Connection", "keep-alive"),
             Map.entry("DNT", "1"),
-            Map.entry("User-Agent",
-                    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:102.0) Gecko/20100101 Firefox/102.0"),
             Map.entry("Accept",
-                    "ext/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8"),
+                    "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8"),
             Map.entry("Accept-Encoding", "gzip, deflate, br"),
             Map.entry("Accept-Language", "en-US,en;q=0.5"),
             Map.entry("TE", "trailers"),
             Map.entry("Upgrade-Insecure-Requests", "1"));
+    private static final String USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64; rv:139.0) Gecko/20100101 Firefox/139.0";
     private static final String ALBUM_DATE_CSS_QUERY =
             ".right-col > table:nth-child(2) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2)";
     private static final String ROOT_URL = "https://metalstorm.net/";
@@ -42,136 +39,158 @@ public class MetalstormClient {
             .toFormatter()
             .withZone(ZoneOffset.UTC);
 
-    private CookieStore cookieStore;
+    private final Playwright playwright;
+    private final Browser browser;
+    private final BrowserContext context;
     private boolean hasFilterBeenSet = false;
 
-    public List<Album> getAlbums(int page) {
-        if (!hasFilterBeenSet) {
-            getDocument(ROOT_URL + "bands/albums.php?filter_show_invisible=1&show_invisible_submit=1");
-            hasFilterBeenSet = true;
+    public MetalstormClient() {
+        this.playwright = Playwright.create();
+        this.browser = playwright.firefox().launch(new BrowserType.LaunchOptions().setHeadless(true));
+        this.context = browser.newContext(
+                new Browser.NewContextOptions().setUserAgent(USER_AGENT).setExtraHTTPHeaders(DEFAULT_HEADERS));
+    }
+
+    private static Album createAlbum(ElementHandle albumRow) {
+        List<ElementHandle> albumColumns = albumRow.querySelectorAll("td");
+
+        if (albumColumns.size() < 8) {
+            throw new RuntimeException("Invalid album row structure");
         }
 
-        List<Album> albums = getDocument(ROOT_URL + "bands/albums.php?page=" + page)
-                .select(".table > tbody:nth-child(2) > tr")
-                .stream()
-                .map(MetalstormClient::createAlbum)
+        ElementHandle artistAlbumColumn = albumColumns.get(2);
+        List<ElementHandle> artistAndAlbumLinks = artistAlbumColumn.querySelectorAll("a");
+
+        List<String> artists = artistAndAlbumLinks.stream()
+                .filter(el -> {
+                    String href = el.getAttribute("href");
+                    return href != null && href.contains("band_id");
+                })
+                .map(ElementHandle::innerText)
                 .toList();
 
-        System.out.printf("Fetched %d albums from page %d of Metalstorm%n", albums.size(), page);
-        try {
-            Thread.sleep(100);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-
-        return albums;
-    }
-
-    public Instant getAlbumDate(String albumId) {
-        return Optional.ofNullable(getDocument(ROOT_URL + "bands/album.php?album_id=" + albumId)
-                        .selectFirst(ALBUM_DATE_CSS_QUERY))
-                .map(Element::text)
-                .map(DATE_TIME_FORMATTER::parse)
-                .map(Instant::from)
-                .orElseThrow(() -> new RuntimeException("Couldn't parse album date"));
-    }
-
-    private Document getDocument(String url) {
-        Connection connection = Jsoup.connect(url).headers(DEFAULT_HEADERS);
-        Document doc;
-
-        if (cookieStore == null) {
-            try {
-                doc = connection.get();
-                cookieStore = doc.connection().cookieStore();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        } else {
-            try {
-                doc = connection.cookieStore(cookieStore).get();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-        return doc;
-    }
-
-    private Document postDocument(String url, String... payload) {
-        Connection connection = Jsoup.connect(url).headers(DEFAULT_HEADERS);
-        Document doc;
-
-        if (cookieStore == null) {
-            try {
-                doc = connection.data(payload).followRedirects(true).post();
-                cookieStore = doc.connection().cookieStore();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        } else {
-            try {
-                doc = connection.cookieStore(cookieStore).data(payload).followRedirects(true).post();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-        return doc;
-    }
-
-    private static Album createAlbum(Element albumRow) {
-        Elements albumColumns = albumRow.getElementsByTag("td");
-        Elements artistAndAlbumNames = albumColumns.get(2).getElementsByTag("a");
-        List<String> artists = artistAndAlbumNames.stream()
-                .filter(el -> el.attr("href").contains("band_id"))
-                .map(Element::text)
+        List<String> artistIds = artistAndAlbumLinks.stream()
+                .filter(el -> {
+                    String href = el.getAttribute("href");
+                    return href != null && href.contains("band_id");
+                })
+                .map(el -> {
+                    String href = el.getAttribute("href");
+                    return href.split("=")[1];
+                })
                 .toList();
-        List<String> artistId = artistAndAlbumNames.stream()
-                .filter(el -> el.attr("href").contains("band_id"))
-                .map(el -> el.attr("href").split("=")[1])
-                .toList();
-        String album = artistAndAlbumNames.stream()
-                .filter(el -> el.attr("href").contains("album_id"))
-                .map(Element::text)
+
+        String album = artistAndAlbumLinks.stream()
+                .filter(el -> {
+                    String href = el.getAttribute("href");
+                    return href != null && href.contains("album_id");
+                })
+                .map(ElementHandle::innerText)
                 .findFirst()
                 .orElseThrow(() -> new RuntimeException("Couldn't find the album"));
-        String albumId = artistAndAlbumNames.stream()
-                .filter(el -> el.attr("href").contains("album_id"))
-                .map(el -> el.attr("href").split("=")[1])
+
+        String albumId = artistAndAlbumLinks.stream()
+                .filter(el -> {
+                    String href = el.getAttribute("href");
+                    return href != null && href.contains("album_id");
+                })
+                .map(el -> {
+                    String href = el.getAttribute("href");
+                    return href.split("=")[1];
+                })
                 .findFirst()
                 .orElseThrow(() -> new RuntimeException("Couldn't find the albumId"));
-        String type = albumColumns.get(4).text();
-        String genre = albumColumns.get(5).text();
-        double rating;
-        int numVotes;
+
+        String type = albumColumns.get(4).innerText();
+        String genre = albumColumns.get(5).innerText();
+
+        double rating = 0.0;
+        int numVotes = 0;
 
         try {
-            Elements ratingElements = albumColumns.get(6).getElementsByTag("a");
-            if (ratingElements.isEmpty()) {
-                rating = 0.0;
-            } else {
-                rating = Double.parseDouble(ratingElements.get(0).text());
+            List<ElementHandle> ratingElements = albumColumns.get(6).querySelectorAll("a");
+            if (!ratingElements.isEmpty()) {
+                String ratingText = ratingElements.getFirst().innerText();
+                rating = Double.parseDouble(ratingText);
             }
-        } catch (NumberFormatException e) {
-            rating = 0.0;
+        } catch (NumberFormatException _) {
         }
 
         try {
-            numVotes = Integer.parseInt(albumColumns.get(7).text());
-        } catch (NumberFormatException e) {
-            numVotes = 0;
+            String votesText = albumColumns.get(7).innerText();
+            numVotes = Integer.parseInt(votesText);
+        } catch (NumberFormatException _) {
         }
 
         return Album.builder()
                 .album(album)
                 .artists(artists)
                 .albumId(albumId)
-                .artistIds(artistId)
+                .artistIds(artistIds)
                 .genre(genre)
                 .type(type)
                 .rating(rating)
                 .votes(numVotes)
                 .build();
+    }
+
+    public List<Album> getAlbums(int pageNumber) {
+        if (!hasFilterBeenSet) {
+            navigateToUrl(ROOT_URL + "bands/albums.php?filter_show_invisible=1&show_invisible_submit=1");
+            hasFilterBeenSet = true;
+        }
+
+        try (Page page = context.newPage()) {
+            navigateToUrl(ROOT_URL + "bands/albums.php?page=" + pageNumber);
+            page.waitForSelector(".table > tbody:nth-child(2) > tr");
+
+            List<ElementHandle> albumRows = page.querySelectorAll(".table > tbody:nth-child(2) > tr");
+            List<Album> albums = albumRows.stream()
+                    .map(MetalstormClient::createAlbum)
+                    .toList();
+
+            System.out.printf("Fetched %d albums from page %d of Metalstorm%n", albums.size(), pageNumber);
+            Thread.sleep(100);
+
+            return albums;
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Instant getAlbumDate(String albumId) {
+        try (Page page = context.newPage()) {
+            navigateToUrl(ROOT_URL + "bands/album.php?album_id=" + albumId);
+            page.waitForSelector(ALBUM_DATE_CSS_QUERY);
+
+            ElementHandle dateElement = page.querySelector(ALBUM_DATE_CSS_QUERY);
+            if (dateElement != null) {
+                String dateText = dateElement.innerText();
+                return DATE_TIME_FORMATTER.parse(dateText, Instant::from);
+            }
+            throw new RuntimeException("Couldn't parse album date");
+        }
+    }
+
+    private void navigateToUrl(String url) {
+        try (Page page = context.newPage()) {
+            page.navigate(url);
+            page.waitForLoadState();
+        }
+    }
+
+    @Override
+    public void close() {
+        if (context != null) {
+            context.close();
+        }
+
+        if (browser != null) {
+            browser.close();
+        }
+
+        if (playwright != null) {
+            playwright.close();
+        }
     }
 }
